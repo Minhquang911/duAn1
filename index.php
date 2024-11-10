cf3030259dd4d9a29eeeabf544564682a600239c
HoangThinhAnh <158267171+HoangThinhAnh@users.noreply.github.com>
2024-11-10 15:33:29 +0700

    Add files via upload
    index_tintuc

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+// Require file Common
+require_once '../commons/env.php'; // Khai báo biến môi trường
+require_once '../commons/function.php'; // Hàm hỗ trợ
+
+// Require toàn bộ file Controllers
+require_once 'controllers/DashboardController.php';
+require_once 'controllers/tinTucControler.php';
+
+
+// Require toàn bộ file Models
+require_once './models/tinTuc.php';
+
+// Route
+$act = $_GET['act'] ?? '/';
+
+// Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
+
+match ($act) {
+    // Dashboards
+    '/'                    => (new DashboardController())->index(),
+
+    // quan ly danh muc san pham 
+    'tin-tucs'             => (new TinTucController())->home(),
+    'form-them-tin-tuc'    => (new TinTucController())->create(),
+    'them-tin-tuc'         => (new TinTucController())->add(),
+    'form-sua-tin-tuc'     => (new TinTucController())->edit(),
+    'sua-tin-tuc'          => (new TinTucController())->update(),
+    'xoa-tin-tuc'          => (new TinTucController())->delete(),
+
+    
+};
